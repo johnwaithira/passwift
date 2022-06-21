50f5c9bbe822dcb1c2b5209fbbfdcc243422e85b
johnwaithira <waithirajon@gmail.com>
2022-06-21 13:23:44 +0300

    Passwift Development

---
+++ app/controller/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+    
+    namespace Waithira\Passwift\app\controller;
+    use Waithira\Passwift\app\core\Controller;
+    
+    class HomeController extends Controller
+    {
+        
+    }
