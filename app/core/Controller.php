9a582f81bd0dc9c2b6cac2ab9731ca21d6fc46a6
johnwaithira <waithirajon@gmail.com>
2022-06-21 13:29:33 +0300

    Password Mananger

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,5 +4,33 @@
     
     class Controller
     {
-        
+        public string $layout = "";
+    
+        /**
+         * @param $layout
+         * @return $this
+         */
+        public function setLayout($layout): static
+        {
+            $this->layout = $layout;
+            return $this;
+        }
+    
+        /**
+         * @param $view
+         * @param $params
+         * @return array|bool|string
+         */
+        public function view($view, $params = []): array|bool|string
+        {
+            return Application::$app->router->view($view, $params);
+        }
+    
+        /**
+         * @return void
+         */
+        public function redirect($path)
+        {
+            return Application::$app->router->redirect($path);
+        }
     }
