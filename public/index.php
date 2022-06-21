25fa39a31867333287bb394b3641d589ad1c5c0b
johnwaithira <waithirajon@gmail.com>
2022-06-21 13:34:26 +0300

    Password Mananger

--- public/index.php
+++ public/index.php
@@ -1,9 +1,70 @@
 <?php
-
+    
+    /*
+    |-------------------------------------------------------------
+    |Load Auto Loader
+    |-------------------------------------------------------------
+    |
+    |
+    */
+    
+    
     require_once __DIR__."/../vendor/autoload.php";
     
-    use Waithira\Passwift\database\Database;
-    $app = new Database();
-    var_dump($app);
+    /*
+    |-------------------------------------------------------------
+    |Load Enviroment /.env file
+    |-------------------------------------------------------------
+    |
+    |
+    */
     
-?>
+    $dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+    
+    /*
+    |-------------------------------------------------------------
+    |Load Database variables
+    |-------------------------------------------------------------
+    |
+    |
+    */
+    
+    $config = [
+        'dsn' => $_ENV['DB_TYPE'].":host=".$_ENV['DB_HOST'].";port=".$_ENV['DB_PORT'].";dbname=".$_ENV['DB_NAME'],
+        'pwd' => $_ENV['DB_PWD'],
+        'user' => $_ENV['DB_USER']
+    ];
+    
+    
+    use Waithira\Passwift\app\core\Application;
+    
+    /*
+    |-------------------------------------------------------------
+    |Instantiate Application
+    |-------------------------------------------------------------
+    |
+    |
+    */
+    
+    $app = new Application(dirname(__DIR__), $config);
+    
+    
+    /*
+    |-------------------------------------------------------------
+    |Load/ Include Routes from web.php file
+    |-------------------------------------------------------------
+    |
+    |
+    */
+    require_once __DIR__."/../route/web.php";
+    
+    /*
+    |-------------------------------------------------------------
+    |Run Application
+    |-------------------------------------------------------------
+    |
+    |
+    */
+    
+    $app->run();
