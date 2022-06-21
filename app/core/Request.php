c3d705dcd374207a0471804231581e5bf6daf45d
johnwaithira <waithirajon@gmail.com>
2022-06-21 13:27:10 +0300

    Password Mananger

--- app/core/Request.php
+++ app/core/Request.php
@@ -4,14 +4,50 @@
     
     class Request
     {
-        public function path()
+        /**
+         * @return mixed|string
+         * @author WaithiraJohn
+         * namespace Waithira\Passwift\app\core
+         */
+    
+        public function path(): mixed
         {
-            
+            $path = $_SERVER['REQUEST_URI'] ?? '/';
+            $position = strpos(
+                $path,
+                '?'
+            );
+        
+            /** @var $position */
+            return $position === false ? $path : substr(
+                $path,
+                0,
+                $position
+            );
         }
     
-        public function method()
+        /**
+         * @return string
+         */
+        public function method(): string
         {
-            
+            return strtolower($_SERVER['REQUEST_METHOD']);
+        }
+    
+        public function inputs(): array
+        {
+            $body = [];
+        
+            foreach($_POST as $key => $val)
+            {
+                /** @var array $body */
+                $body[$key] = filter_input(
+                    INPUT_POST,
+                    $key,
+                    FILTER_SANITIZE_SPECIAL_CHARS
+                );
+            }
+            return $body;
         }
         
     }
