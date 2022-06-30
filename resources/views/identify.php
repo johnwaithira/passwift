4d1d0faf10fd9f58f1c68397802eaa11734066c9
johnwaithira <waithirajon@gmail.com>
2022-07-01 00:15:08 +0300

    Passwift

--- resources/views/identify.php
+++ resources/views/identify.php
@@ -3,7 +3,14 @@
 
 
 ?>
-
+<style>
+    hr{
+        border-top: 1px solid #ccd0d5;
+    }
+    input:focus{
+        border: 2px solid green;
+    }
+</style>
 <div class="p-20">
     <div class="col-5 col-m-6 col-s-9 m-a">
         <div class="box-shadow b-r-4">
@@ -12,11 +19,20 @@
                     <p class="f-w-600 f-s-20 c-black">Find Your Account</p>
                 </div>
             </div>
-            <hr>
+            <hr style="color:#ccd0d5;">
             <div class="p-20">
+                <div class="error-div">
+
+                </div>
                 <p class="f-s-18">Please enter your email address or your username to search for your account.</p>
             </div>
+            <div class="p-l-20 p-r-20 p-b-20" >
+                <input type="text" class="p-15 w-p-100 b-r-6" style="border: 1px solid #ccd0d5;">
+            </div>
+            <hr>
+            <div class="p-20">
 
+            </div>
         </div>
     </div>
 </div>
