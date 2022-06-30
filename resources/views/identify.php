353e172950a7a01b47a6638841a90ef103d58d5a
johnwaithira <waithirajon@gmail.com>
2022-06-30 23:56:28 +0300

    Passwift

--- resources/views/identify.php
+++ resources/views/identify.php
@@ -6,7 +6,7 @@
 
 <div class="p-20">
     <div class="col-5 col-m-6 col-s-9 m-a">
-        <div class="box-shadow">
+        <div class="box-shadow b-r-4">
             <div class="p-20-10">
 
             </div>
