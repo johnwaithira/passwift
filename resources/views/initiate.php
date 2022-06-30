43be996229c3516c9c9c1354fbcb410390999948
johnwaithira <waithirajon@gmail.com>
2022-07-01 00:43:49 +0300

    Passwift

--- resources/views/initiate.php
+++ resources/views/initiate.php
@@ -25,17 +25,15 @@
                     <p class="f-w-600 f-s-18 c-black">Reset Your Password</p>
                 </div>
             </div>
-            <hr style="color:#ccd0d5;">
             <div class="p-20">
                 <div class="error-div">
 
                 </div>
                 <p class="f-s-16">Please enter your email address or your username to search for your account.</p>
             </div>
-            <div class="p-l-20 p-r-20 p-b-20" >
+            <div class="p-l-20 p-r-20 p-b-20" id="hr">
                 <input type="text" class="p-17 w-p-100 b-r-6" style="border: 1px solid #ccd0d5;" placeholder="Enter your email address or your username">
             </div>
-            <hr>
             <div class="p-15-20">
                 <div class="btns text-right">
                     <button class="b-n p-7-18 b-r-6 f-w-800 f-s-16" style="background: #e4e6eb;">Cancel</button>
