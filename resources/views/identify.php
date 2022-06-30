9ecfc2d5e80d8399b5c141055be841f9079a82c0
johnwaithira <waithirajon@gmail.com>
2022-07-01 00:39:39 +0300

    Passwift

--- resources/views/identify.php
+++ resources/views/identify.php
@@ -11,7 +11,7 @@
         outline: none;
     }
     ::placeholder{
-        font-size: 18px;
+        font-size: 16px;
     }
 </style>
 <div class="p-20">
@@ -19,7 +19,7 @@
         <div class="box-shadow b-r-4">
             <div class="p-20">
                 <div class="form">
-                    <p class="f-w-600 f-s-20 c-black">Find Your Account</p>
+                    <p class="f-w-600 f-s-18 c-black">Find Your Account</p>
                 </div>
             </div>
             <hr style="color:#ccd0d5;">
@@ -27,7 +27,7 @@
                 <div class="error-div">
 
                 </div>
-                <p class="f-s-18">Please enter your email address or your username to search for your account.</p>
+                <p class="f-s-16">Please enter your email address or your username to search for your account.</p>
             </div>
             <div class="p-l-20 p-r-20 p-b-20" >
                 <input type="text" class="p-17 w-p-100 b-r-6" style="border: 1px solid #ccd0d5;" placeholder="Enter your email address or your username">
