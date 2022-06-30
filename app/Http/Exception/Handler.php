fbf1b4da12f4734fc19b10dc83771e570356c1c9
johnwaithira <waithirajon@gmail.com>
2022-06-30 15:48:49 +0300

    Passwift

--- app/Http/Exception/Handler.php
+++ app/Http/Exception/Handler.php
@@ -13,7 +13,9 @@
         public function _404(): bool|array|string
         {
             Application::$app->controller->setLayout('layouts.app');
+            Application::$app->router->resources('layouts.navigation');
             Application::$app->router->response->setResposeCode(404);
-            return $this->view('errors.404');
+            return $this->view('errors.404', ['url' => $_SERVER['REQUEST_URI']]);
+
         }
     }
--- resources/views/layouts/head.php
+++ resources/views/layouts/head.php
@@ -7,7 +7,6 @@ use Waithira\Passwift\app\core\Application;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://127.0.0.1/shoetailor/ke/css/style.css">
     <link rel="stylesheet" href="/css/popup.css">
     <link rel="stylesheet" href="/css/style.css">
 </head>
