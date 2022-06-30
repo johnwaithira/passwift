c06403adc0ac9743c85bbc7b7a9cf93e3a894e42
johnwaithira <waithirajon@gmail.com>
2022-06-30 15:57:52 +0300

    Passwift

--- resources/views/errors/404.php
+++ resources/views/errors/404.php
@@ -1,11 +1,18 @@
 <div class="w-p-100 m-t-10">
    <div class="p-20">
-        <div class="col-4 m-a box-shadow b-r-10">
+        <div class="col-4 m-a box-shadow1">
             <div class="p-20 text-center">
                 <p class="color_fade f-s-73 p-b-20">404!</p>
                 <h2 class="p-b-40 color_fade ">Page not found</h2>
-        
+                <?php echo $url;?>
+
             </div>
         </div>
    </div>
+</div>
+
+<div class="text-center">
+    <a href="/">
+        <button class="b-one bg-inherit p-7-10 f-s-20 b-r-5" style="background: blue; color:white;">Back home</button>
+    </a>
 </div>
