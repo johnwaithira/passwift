f60c3f44b9b76a4e944b0c09a63a835865798583
johnwaithira <waithirajon@gmail.com>
2022-07-01 12:49:54 +0300

    Passwift

---
+++ resources/views/login.php
@@ -0,0 +1,55 @@
+<title>Forgotten Password | Can't Login to Passwift</title>
+<?php
+
+use Waithira\Passwift\app\core\Application;
+
+
+?>
+<style>
+    .links{
+        color: blue;
+    }
+    .links a{
+       color: #1877f2;
+       padding-left: 3px;
+       font-size: 14px;
+    }
+    .links a:hover{
+        text-decoration: underline;
+        transition: 0.2s;
+    }
+</style>
+<div class="p-20">
+    <div class="col-4 col-m-6 col-s-8 m-a">
+        <div class=" text-center p-20-0">
+            <img class="w-150 m-a" src="https://johnwaithira.github.io/passwift/passwift-removebg-preview.png">
+
+        </div>
+        <div class="box-shadow b-r-6">
+            <div class="p-20" >
+                <div class="form">
+                    <h2 style="color: #162643;" class="f-s-18 c-black text-center">Login to Passwift</h2>
+                </div>
+            </div>
+            <div class="p-10-20">
+                <input type="text" class="p-15-17 w-p-100 b-r-6" style="border: 1px solid #ccd0d5;" placeholder="Enter your email address or your username">
+            </div>
+
+            <div class="p-l-20 p-r-20 p-b-10">
+                <input type="text" class="p-15-17 w-p-100 b-r-6" style="border: 1px solid #ccd0d5;" placeholder="Password">
+            </div>
+            <div class="p-l-20 p-r-20  p-b-20 w" style="">
+                <div class="">
+                    <button class="b-n p-10-18 w-p-100 b-r-6 f-w-800 f-s-16"style="background: #216fdb; color:white;">Login</button>
+                </div>
+            </div>
+            <div class="p-l-20 p-r-20 p-b-30">
+                <div class="text-center links">
+                    <a href="">Forgotten account?</a>
+                    <a href="">Sign up for Passwift</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<p></p>
